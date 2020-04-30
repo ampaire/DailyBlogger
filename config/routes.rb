0464b8a49f91f24cb370cc664f41bb147bb4a8e1
ampaire <femayapr@gmail.com>
2020-04-29 21:02:23 -0600

    Authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :room_messages
   resources :rooms
   devise_for :users
-  get 'blogger/index'
+   root controller: :rooms, action: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

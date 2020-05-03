8af1c19edeba5d9c96935cf17cbdf66a1a7ab152
ampaire <femayapr@gmail.com>
2020-05-02 18:23:35 -0600

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root controller: :rooms, action: :index
 
   resources :room_messages
-  resources :rooms, only: %i[create show update] 
+  resources :rooms, only: %i[index create show update] 
 end
